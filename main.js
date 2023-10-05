4fb54b43d196247c111568ce013b479c966a521e
geuuuu <83645225+emirhan-uysal@users.noreply.github.com>
2023-10-05 22:57:05 +0300

    Initial commit
    Created the basic structure of the application

---
+++ main.js
@@ -0,0 +1,32 @@
+window.onload = init;
+function init(){
+    const map = new ol.Map({
+        view : new ol.View({
+            center: [3245075.5956414873, 5008280.403576283],
+            zoom: 17,
+            maxZoom: 20
+        }),
+        layers: [
+            new ol.layer.Tile({
+                source: new ol.source.OSM()
+            })
+        ],
+        target: 'js-map'
+    })
+
+    map.on('click',function(e){
+        console.log(e.coordinate)
+    })
+
+    //Adding Vector Layers
+    const buildingsGeoJSON = new ol.layer.VectorImage({
+        source: new ol.source.Vector({
+            url: './data/map.geojson',
+            format: new ol.format.GeoJSON()
+        }),
+        visible: true,
+        title: 'buildingsGeoJSON'
+    })
+
+    map.addLayer(buildingsGeoJSON);
+}
