af3df9a7510ae5d70979fcb654365d62e1145fa6
Okan Bulgur <okanbulgur@gmail.com>
2023-10-08 23:08:16 +0300

    Update new version

--- buildingStyle.js
+++ buildingStyle.js
@@ -26,27 +26,90 @@ let populationRanges = [
     }
 ]
 
+function getColorByPopulation(population){
+    for(var i=0; i<populationRanges.length; i++){
+        if(population >= populationRanges[i].MinPop){
+            return populationRanges[i].Color;
+        }
+    }
+    return null;
+}
+
+function getStyleByPopulation(population){
+    var fillColor = getColorByPopulation(population);
+    var strokeColor = "black";
+    var strokeWidth = 1.2;
+    var text = population.toString();
+    var textFont = "bold 10px serif";
+
+    const style = new ol.style.Style({
+        fill: new ol.style.Fill({
+            color: fillColor,
+        }),
+        stroke: new ol.style.Stroke({
+            color: strokeColor,
+            width: strokeWidth
+        }),
+        text: new ol.style.Text({
+            text: text,
+            font: textFont
+        })
+    });
+    return style;
+}
+
 function drawShapesOnMap(path, map){
     fetch(path)
     .then(response => response.json())
     .then(data => {
-        for(var i=0; i<data.features.length; i++){
-            var population = data.features[i].properties.Population;
-            
-            var vectorSource = new ol.source.Vector({
-                features: new ol.format.GeoJSON().readFeatures(data.features[i])
+        const format = new ol.format.GeoJSON();
+        const features = format.readFeatures(data)
+        let style, population;
+
+        let vectorLayer = new ol.layer.Vector({
+            source: new ol.source.Vector(),
+            opacity: 0.8,
+            visible: true,
+            zIndex: 2,
+            title: "VectorLayer"
+        });
+
+        features.forEach((feature) => {
+            population = feature.getProperties().Population;
+            style = getStyleByPopulation(population);
+
+            let newFeature = new ol.Feature({
+                type: "Feature",
+                geometry: {type: "Polygon", coordinates: new ol.geom.Polygon([feature.getGeometry().getCoordinates()])},
+                Population: feature.getProperties().Population,
+                Name: feature.getProperties().Name,
             });
 
-            console.log(vectorSource);
-
-            const vectorLayer = new ol.layer.Vector({
-                source: vectorSource
-            });
-
-            console.log(vectorLayer);
-
-            map.addLayer(vectorLayer);
-        }
+            newFeature.setStyle(style);
+            vectorLayer.getSource().addFeatures([newFeature]);
+        });
+        map.addLayer(vectorLayer);
+    
+        let layers = map.getLayers();
+            layers.forEach(function(layer, i) {
+                console.log("Layer "+ i + ": ",layer.get('title'));
+                if (layer.get('title') != "StandartLayer"){
+                    console.log("Features:");
+                    layer.getSource().getFeatures().forEach((f, i) => {
+                        console.log("Index: "+i,"\nFeature: ",f,"\nProperties: ",f.getProperties(),"\nPopulation: ",f.getProperties().Population ,"\nColor: ",f.getStyle().getFill().getColor(), "\nCoordinates: ", f.getGeometry().getCoordinates()) 
+                    });  
+                }
+        });
     })
     .catch(error => console.error('Error:', error));
+}
+
+function changeStyleByPopulation(features){
+    //var features = layer.getSource().getFeatures();
+    console.log(features);
+    features.forEach(feature => {
+        let population = feature.getProperties().Population;
+        let style = getStyleByPopulation(population);
+        feature.setStyle(style)
+    })
 }
--- main.js
+++ main.js
@@ -6,27 +6,43 @@ function init(){
             zoom: 17,
             maxZoom: 20
         }),
-        layers: [
-            new ol.layer.Tile({
-                source: new ol.source.OSM()
-            })
-        ],
         target: 'js-map'
-    })
+    });
+
+    const standartLayer = new ol.layer.Tile({
+        source: new ol.source.OSM(),
+        visible: true,
+        zIndex: 1,
+        title: "StandartLayer"
+    });
+
+    map.addLayer(standartLayer);
 
     map.on('click',function(e){
         console.log(e.coordinate)
-    })
+    });
 
-    //Adding Vector Layers
-    const buildingsGeoJSON = new ol.layer.VectorImage({
+    let buildingsGeoJSON = new ol.layer.VectorImage({
         source: new ol.source.Vector({
             url: './data/map.geojson',
             format: new ol.format.GeoJSON()
         }),
+        opacity: 0.8,
         visible: true,
-        title: 'buildingsGeoJSON'
-    })
-    //map.addLayer(buildingsGeoJSON);
-    drawShapesOnMap("./data/map.geojson",map)
+        zIndex: 1,
+        title: 'buildingsGeoJSON',
+    });
+
+    var features = buildingsGeoJSON.getSource().getFeatures();
+    console.log(buildingsGeoJSON);
+    changeStyleByPopulation(features);
+    map.addLayer(buildingsGeoJSON)
+    //buildingsGeoJSON.setStyle(getStyleByPopulation(100));
+
+    //drawShapesOnMap("./data/map.geojson", map);
+
+    var layers = map.getLayers();
+        layers.forEach(function(layer) {
+        console.log("main: ",layer.get('title'));
+    });
 }
