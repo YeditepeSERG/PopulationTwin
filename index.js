3039208770c5d6b6580152693fc928c60be89539
erentasken <eren.tasken@std.yeditepe.edu.tr>
2023-10-17 17:31:21 +0300

    hellalujahhahahhahah

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const fs = require('fs');
+
+const app = express();
+
+express.static.mime.define({'application/javascript': ['']})
+app.use(express.static('public'));
+app.use(express.static('inputPopup'));
+app.get('/', (request, response) => {
+
+    fs.readFile('./index.html', 'utf8', (err,html) => {
+        if (err){
+            response.status(500).send(':(');
+        }
+        response.send(html);
+    })
+
+});
+
+app.get('/about.html', (request, response) => {
+
+    fs.readFile('./about.html', 'utf8', (err,html) => {
+        if (err){
+            response.status(500).send(':(');
+        }
+        response.send(html);
+    })
+
+});
+
+app.listen(process.env.PORT || 3000, ()=> console.log('App avaliable on http://localhost:3000'))
