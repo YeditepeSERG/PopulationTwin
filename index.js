20d4d3dfaa0c812429e0eab19e4606144688c9ba
erentasken <eren.tasken@gmail.com>
2023-10-18 14:33:08 +0300

    mime fixed

--- index.js
+++ index.js
@@ -3,11 +3,9 @@ const fs = require('fs');
 
 const app = express();
 
-express.static.mime.define({'application/javascript': ['']})
 app.use(express.static('.'));
 app.use(express.static('inputPopup'));
 app.get('/', (request, response) => {
-
     fs.readFile('./index.html', 'utf8', (err,html) => {
         if (err){
             response.status(500).send(':(');
