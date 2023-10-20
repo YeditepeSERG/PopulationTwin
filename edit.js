6f98d773c6dc265f1e8868436c5b6b93ce91a2ac
Fevzi Babaoğlu <fevzibabaoglu03@gmail.com>
2023-10-20 10:22:47 +0300

    added logIn check to edit button

--- edit.js
+++ edit.js
@@ -1,15 +1,5 @@
-let draw;
-
-document.getElementById("editToggle").onclick = () => {
-    if (document.getElementById("editToggle").checked) {
-        addInteraction();
-    } else {
-        map.removeInteraction(draw)
-    }
-}
-
+// helper function
 function splitArrayIntoPairs(arr, chunkSize) {
-  
     return arr.reduce(function (result, item, index) {
         if (index % chunkSize === 0) {
             result.push(arr.slice(index, index + chunkSize));
@@ -18,6 +8,21 @@ function splitArrayIntoPairs(arr, chunkSize) {
     }, []);
 }
 
+let editToggleButton = document.getElementById("editToggle"); 
+editToggleButton.onclick = () => {
+    if (!loggedIn) {
+        editToggleButton.checked = false;
+        alert("You are not logged in!")
+    }
+
+    if (editToggleButton.checked) {
+        addInteraction();
+    } else {
+        map.removeInteraction(draw);
+    }
+}
+
+let draw;
 const source = new ol.source.Vector({wrapX: false});
 
 function addInteraction() {
@@ -32,23 +37,19 @@ function addInteraction() {
         let coords = feature.getGeometry().flatCoordinates;
         let xy_coords = splitArrayIntoPairs(coords, 2);
         xy_coords.pop();
-
         xy_coords.forEach((element, i) => {
             xy_coords[i] = ol.proj.transform(element, 'EPSG:3857', 'EPSG:4326');
         });
 
-        let newBuilding = new Building("test", "test name", 400);
+        let newBuilding = new Building("test", "test name", 400);   //! fix this
         addNewBuildingToJson(newBuilding, [xy_coords]);
-
-        setTimeout(() => {
-            init();
-        }, 100)
+        setTimeout(init, 100)
     });
 }
 
-/*
+/* 
+! implement undo button/function
 document.getElementById('undo').addEventListener('click', function () {
   draw.removeLastPoint();
 });
 */
-
